0ce291d862a7194cf8f93efb244b38ba20a0d75c
Benjamin <benjamin.villarroel3@gmail.com>
2023-10-02 11:32:15 -0400

    feat: DetalleOrden methods

--- tarea1/src/main/java/org/example/DetalleOrden.java
+++ tarea1/src/main/java/org/example/DetalleOrden.java
@@ -2,22 +2,24 @@ package org.example;
 
 public class DetalleOrden {
     private int cantidad;
-    public DetalleOrden(){
-        ;
+    public DetalleOrden(int cantidad){
+        cantidad = this.cantidad;
     }
     public float calcPrecio(Articulo a) {
         float precio = a.getPrecio() * cantidad;
-        precio = precio * 1.19; // float o double ? D:
+        precio = precio * 1.19f; // float o double ? D:
         return precio;
     }
     public float calcPrecioSinIVA(Articulo a) {
         float precio = a.getPrecio() * cantidad;
         return precio;
     }
-    public float calcIVA() {
-        return 0;
+    public float calcIVA(Articulo a) {
+        float iva = a.getPrecio() * 0.19f;
+        return iva;
     }
-    public float calcPeso() {
-        return 0;
+    public float calcPeso(Articulo a) {
+        float peso = a.getPeso() * this.cantidad;
+        return peso;
     }
 }
