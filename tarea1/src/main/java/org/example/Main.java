bb9d33c56da2b246856b0392b241dd73631fa180
Benjamin <benjamin.villarroel3@gmail.com>
2023-10-04 19:40:27 -0400

    Added composition in OrdenCompra

--- tarea1/src/main/java/org/example/Main.java
+++ tarea1/src/main/java/org/example/Main.java
@@ -7,6 +7,6 @@ public class Main {
         Articulo rueda = new Articulo("Rueda", "Gira",10, 70000);
         Cliente c1 = new Cliente("David","21123456-k");
         OrdenCompra compra1 = new OrdenCompra();
-        Date fecha = new Date(); // No se como funciona Date
+        Date fecha = new Date(2023,10,4);
     }
 }
--- tarea1/src/main/java/org/example/OrdenCompra.java
+++ tarea1/src/main/java/org/example/OrdenCompra.java
@@ -5,12 +5,30 @@ import java.util.Date;
 public class OrdenCompra {
     private Date fecha;
     private String estado;
+    private DetalleOrden detallito;
     public OrdenCompra() {
         fecha = new Date();
         estado = new String();
     }
+
+    public DetalleOrden getDetallito() {
+        return detallito;
+    }
+
+    public void setDetallito(DetalleOrden detallito) {
+        this.detallito = detallito;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
     public float calcPrecioSinIVA() {
-        return 0;
+        ;
     }
     public float calcIVA() {
         return 0;
